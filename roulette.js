8f19e8b261e3162a94f93150aa7e073b32987efd
ExtremeAntonis <68855179+ExtremeAntonis@users.noreply.github.com>
2024-01-06 02:06:33 +0200

    Update roulette.js

--- roulette.js
+++ roulette.js
@@ -5,10 +5,25 @@ class RouletteBetting {
     }
 
     placeBet(name, numbers, betAmount) {
-        numbers = numbers.split(',').map(num => parseInt(num, 10));
-        betAmount = parseInt(betAmount, 10);
-        this.bets.push({ name, numbers, betAmount });
+        if (name === 'red' || name === 'black' || name === 'even' || name === 'odd') {
+            this.placeSpecialBet(name, betAmount);
+        } else {
+            numbers = numbers.split(',').map(num => parseInt(num, 10));
+            betAmount = parseInt(betAmount, 10);
+            this.bets.push({ name, numbers, betAmount });
+        }
         this.updateCurrentBetsDisplay();
+    }
+
+    placeSpecialBet(name, betAmount) {
+        const specialBets = ['red', 'black', 'even', 'odd'];
+
+        if (!specialBets.includes(name)) {
+            console.error(`Invalid special bet: ${name}`);
+            return;
+        }
+
+        this.bets.push({ name, betAmount });
     }
 
     calculateWinnings(winningNumber) {
@@ -24,19 +39,43 @@ class RouletteBetting {
 
         this.bets.forEach(bet => {
             const { name, numbers, betAmount } = bet;
-            if (numbers.includes(winningNumber)) {
-                const payoutRatio = this.getPayoutRatio(numbers.length);
-                const winnings = betAmount * payoutRatio + betAmount; // Include original bet
-                winners.push({ name, winnings });
-                totalPayout += winnings;
+            if (numbers) {
+                if (numbers.includes(winningNumber)) {
+                    const payoutRatio = this.getPayoutRatio(numbers.length);
+                    const winnings = betAmount * payoutRatio + betAmount; // Include original bet
+                    winners.push({ name, winnings });
+                    totalPayout += winnings;
+                }
+            } else {
+                // Handle special bets (red, black, even, odd)
+                if (this.checkSpecialBetResult(name, winningNumber)) {
+                    const winnings = betAmount * 2; // Special bets have a payout ratio of 2
+                    winners.push({ name, winnings });
+                    totalPayout += winnings;
+                }
             }
         });
 
-        this.houseProfit += (this.totalBetAmount() - totalPayout);
+        this.houseProfit += this.totalBetAmount() - totalPayout;
         this.bets = []; // Clear current bets
         this.updateWinningBetsDisplay(winners);
         this.updateHouseProfitDisplay();
         this.updateCurrentBetsDisplay(); // Clear display of current bets
+    }
+
+    checkSpecialBetResult(name, winningNumber) {
+        switch (name) {
+            case 'red':
+                return winningNumber % 2 !== 0;
+            case 'black':
+                return winningNumber % 2 === 0;
+            case 'even':
+                return winningNumber % 2 === 0 && winningNumber !== 0;
+            case 'odd':
+                return winningNumber % 2 !== 0;
+            default:
+                return false;
+        }
     }
 
     getPayoutRatio(numNumbers) {
@@ -50,12 +89,18 @@ class RouletteBetting {
     }
 
     totalBetAmount() {
-        return this.bets.reduce((acc, bet) => acc + bet.betAmount, 0);
+        return this.bets.reduce((acc, bet) => acc + (bet.betAmount || 0), 0);
     }
 
     updateCurrentBetsDisplay() {
         const display = document.getElementById('currentBets');
-        display.innerHTML = this.bets.map(bet => `${bet.name}: ${bet.numbers.join(', ')} - $${bet.betAmount}`).join('<br>');
+        display.innerHTML = this.bets.map(bet => {
+            if (bet.numbers) {
+                return `${bet.name}: ${bet.numbers.join(', ')} - $${bet.betAmount}`;
+            } else {
+                return `${bet.name} - $${bet.betAmount}`;
+            }
+        }).join('<br>');
     }
 
     updateWinningBetsDisplay(winners) {
